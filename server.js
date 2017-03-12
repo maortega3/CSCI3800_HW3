17c1bc9f7e74d180b6d14ef1438da6f0a9bbe549
maortega3 <maortega3@outlook.com>
2017-03-11 20:37:00 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 // <mark.ortega@ucdenver.edu>
-// Assignment for web api 3800. Main Proxy
+// Assignment for web api 3800. Main Proxy (Lacking Functionality Especially in error handling..)
 var express = require('express');
 var app = express();
 var localPort = 9000;
-// Base route
+// A Call to the Base route will instantiate an instance of gihub
 app.get('/', function (req, res) {
     console.log("Ping home");
-    
     //We have a GET to our base url now we will create our github variables  
     var GitHubApi = require("github");  
     var github = new GitHubApi({
@@ -14,28 +13,22 @@ app.get('/', function (req, res) {
     });
 
 
-    var token =  "THE TOKEN WOULD GO HERE"; //DON'T STORE ON GITHUB!
-
+    var token =  "THE TOKEN WOULD GO HERE"; //DON'T STORE ON GITH
     github.authenticate({
         type: "oauth",
         token: token
     });
 
     github.user.get({ user: 'maortega3'} , function(err, res) {
-        
         console.log("GOT ERR?", err);
-        console.log("GOT RES?", resp);
+        console.log("GOT RES?", res);
         res.send(res);
-
-        
     });
 })
 
 // The server is listnening
 var server = app.listen(localPort, function () {
-
     var host = server.address().address
     var port = server.address().port
-
-    console.log("Example app listening at http://%s:%s", host, port)
+    console.log("We are on: ", host, port)
 }) 
